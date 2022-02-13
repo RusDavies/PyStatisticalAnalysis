e83857b21ddd46ce68ea46bc2aef1c4bdd85aa60
RusDavies <russell@blakemere.ca>
2022-02-13 15:32:53 -0500

    Initial slurp

---
+++ ChangePointDetection/CPD_R.py
@@ -0,0 +1,17 @@
+
+from rpy2.robjects import FloatVector
+from rpy2.robjects.packages import importr
+
+class CPD_R:
+    def __init__(self):
+        self.cpm = importr('cpm')
+
+    @staticmethod
+    def robj_to_dict(robj):
+        return dict(zip(robj.names, map(list, robj)))
+
+    def detectChanges(self, data, cpmType='GLR',  ARL0=2000, startup=20):
+        data = FloatVector(data)
+        result = self.cpm.processStream(data, cpmType='GLR',  ARL0=2000, startup=20)
+        result = CpdR.robj_to_dict(result)
+        return result 
